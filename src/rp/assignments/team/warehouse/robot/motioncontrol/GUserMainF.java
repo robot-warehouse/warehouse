db068a1a0121a3a4d26e0b7e5d2640befb632405
axm1415 <axm1415@cs.bham.ac.uk>
2018-03-04 16:03:17 +0000

    Upload New File

---
+++ src/rp/assignments/team/warehouse/robot/motioncontrol/GUserMainF.java
@@ -0,0 +1,97 @@
+import java.util.Queue;
+
+import lejos.nxt.Button;
+import lejos.nxt.ButtonListener;
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+import lejos.robotics.subsumption.Arbitrator;
+import lejos.robotics.subsumption.Behavior;
+import lejos.util.Delay;
+import rp.config.RobotConfigs;
+
+public class GUserMainF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue<String> path = new Queue<String>();
+		System.out.println("Select path");
+
+		Button.ESCAPE.addButtonListener(new ButtonListener() {
+
+			@Override
+			public void buttonReleased(Button _b) {
+
+			}
+
+			@Override
+			public void buttonPressed(Button _b) {
+
+				LightSensor lightSensor1 = new LightSensor(SensorPort.S1);
+				LightSensor lightSensor2 = new LightSensor(SensorPort.S4);
+				int white, black, white2, black2 = 0;
+				Button.waitForAnyPress();
+				System.out.println("Light colour");
+				Button.waitForAnyPress();
+				white = lightSensor1.getNormalizedLightValue();
+				white2 = lightSensor1.getNormalizedLightValue();
+				int averageWhite = (white + white2) / 2;
+				System.out.println("White colour: " + averageWhite);
+				System.out.println("Dark colour");
+				Button.waitForAnyPress();
+				lightSensor1.calibrateLow();
+				lightSensor2.calibrateLow();
+				black = lightSensor1.getNormalizedLightValue();
+				black2 = lightSensor2.getNormalizedLightValue();
+				int averageBlack = (black + black2) / 2;
+				System.out.println("Black colour: " + averageBlack);
+
+				int average = (averageBlack + averageWhite) / 2;
+				Button.waitForAnyPress();
+
+				Behavior lineFollower = new GForwardLine(RobotConfigs.EXPRESS_BOT, average);
+				Behavior rotateC = new GRotateMotorC(RobotConfigs.EXPRESS_BOT, average);
+				Behavior rotateA = new GRotateMotorA(RobotConfigs.EXPRESS_BOT, average);
+				Behavior conjunction = new GUserConjunct(RobotConfigs.EXPRESS_BOT, average, path);
+				Behavior[] bArray = {conjunction, lineFollower,rotateC, rotateA};
+				Arbitrator arby = new Arbitrator(bArray);
+				arby.start();
+
+			}
+		});
+
+		Button.LEFT.addButtonListener(new ButtonListener() {
+
+			@Override
+			public void buttonReleased(Button _b) {
+
+			}
+
+			@Override
+			public void buttonPressed(Button _b) {
+				path.addElement("LEFT");
+				System.out.println("Added Left");
+
+			}
+		});
+
+		Button.RIGHT.addButtonListener(new ButtonListener() {
+
+			@Override
+			public void buttonReleased(Button _b) {
+
+			}
+
+			@Override
+			public void buttonPressed(Button _b) {
+				path.addElement("RIGHT");
+				System.out.println("Added Right");
+
+			}
+		});
+
+		while (true) {
+			Delay.msDelay(100);
+		}
+	}
+
+}
