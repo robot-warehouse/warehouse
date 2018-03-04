057c13c987b98b0b22774f7cb01ace40d5875a03
ajc785 <ajc785@student.bham.ac.uk>
2018-03-04 17:17:29 +0000

    Added missed changes to Controller class

--- src/rp/assignments/team/warehouse/server/Controller.java
+++ src/rp/assignments/team/warehouse/server/Controller.java
@@ -1,4 +1,38 @@
 package rp.assignments.team.warehouse.server;
 
-public class Controller {
+import rp.assignments.team.warehouse.server.job.selection.Location;
+
+import java.io.File;
+import java.util.Map;
+// TODO
+public class Controller implements IController {
+    @Override
+    public void loadFile(File file) {
+
+    }
+
+    @Override
+    public void setupConnections() {
+
+    }
+
+    @Override
+    public void disconnectRobot() {
+
+    }
+
+    @Override
+    public void cancelCurrentJob() {
+
+    }
+
+    @Override
+    public Map<String, Location> getRobotLocations() {
+        return null;
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
 }
