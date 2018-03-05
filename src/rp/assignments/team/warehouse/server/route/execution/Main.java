5018dcc0cec929bc88fe957d1d2d92c7b2676ef4
axm1415 <axm1415@cs.bham.ac.uk>
2018-03-05 23:21:33 +0000

    Delete Main.java

--- src/rp/assignments/team/warehouse/server/route/execution/Main.java
+++
@@ -1,8 +0,0 @@
-package RobotMotionControl;
-
-public static void main(String[] args){
-		
-		RobotProgrammingDemo rp = new RobotMotionControl(RobotConfigs.EXPRESS_BOT);
-		rp.run();
-		Button.waitForAnyPress();
-	}
