330e0f23b8a069736e9ab3f9cc222f3b17fe827a
ajc785 <ajc785@git.cs.bham.ac.uk>
2018-03-06 20:30:39 +0000

    Moved Route plan tests into separate package
    Also separated into individual tests

--- src/rp/assignments/team/warehouse/test/route/RoutePlanningTest.java
+++ src/rp/assignments/team/warehouse/test/route/RoutePlanningTest.java
@@ -1,18 +1,22 @@
-package rp.assignments.team.warehouse.server.route.planning;
+package rp.assignments.team.warehouse.test.route;
 
+import org.junit.Assert;
 import org.junit.Test;
 import rp.assignments.team.warehouse.server.Location;
+import rp.assignments.team.warehouse.server.route.planning.AStar;
+import rp.assignments.team.warehouse.server.route.planning.State;
 
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class RoutePlanningTest {
 
     @Test
-    public void test() {
+    public void simpleHorizontalTest() {
         // rudimentary horizontal tests.
-        ArrayList<Location> result1 = new ArrayList<>();
+        ArrayList<Location> result1;
         Location start1 = new Location(0, 0);
         Location goal1 = new Location(5, 0);
         result1 = AStar.findPath(start1, goal1);
@@ -27,9 +31,12 @@ public class RoutePlanningTest {
             }
         };
         assertEquals(expected1, result1);
+    }
 
+    @Test
+    public void simpleVerticalTest() {
         // rudimentary vertical tests.
-        ArrayList<Location> result2 = new ArrayList<>();
+        ArrayList<Location> result2;
         Location start2 = new Location(0, 0);
         Location goal2 = new Location(0, 5);
         result2 = AStar.findPath(start2, goal2);
@@ -44,37 +51,45 @@ public class RoutePlanningTest {
             }
         };
         assertEquals(expected2, result2);
+    }
 
+    @Test
+    public void startPosValidationTest() {
         // check validation of starting position
-        ArrayList<Location> result3 = new ArrayList<>();
+        ArrayList<Location> result3;
         Location start3 = new Location(-1, 0);
         Location goal3 = new Location(0, 5);
         result3 = AStar.findPath(start3, goal3);
-        ArrayList<Location> expected3 = null;
-        assertEquals(expected3, result3);
+        assertNull(result3);
+    }
 
+    @Test
+    public void goalPosValidationTest() {
         // check validation of goal position
-        ArrayList<Location> result4 = new ArrayList<>();
+        ArrayList<Location> result4;
         Location start4 = new Location(-1, 0);
         Location goal4 = new Location(0, 5);
         result4 = AStar.findPath(start4, goal4);
-        ArrayList<Location> expected4 = null;
-        assertEquals(expected4, result4);
+        assertNull(result4);
+    }
 
+    @Test
+    public void startPosIsObstacleTest() {
         //check if an obstacle is rejected as starting position
-        ArrayList<Location> result5 = new ArrayList<>();
+        ArrayList<Location> result5;
         Location start5 = new Location(1, 2);
         Location goal5 = new Location(0, 5);
         result5 = AStar.findPath(start5, goal5);
-        ArrayList<Location> expected5 = null;
-        assertEquals(expected5, result5);
+        assertNull(result5);
+    }
 
+    @Test
+    public void goalPosIsObstacleTest() {
         //check if an obstacle is rejected as goal position
-        ArrayList<Location> result6 = new ArrayList<>();
+        ArrayList<Location> result6;
         Location start6 = new Location(0, 0);
         Location goal6 = new Location(1, 2);
         result6 = AStar.findPath(start6, goal6);
-        ArrayList<Location> expected6 = null;
-        assertEquals(expected6, result6);
+        assertNull(result6);
     }
 }
