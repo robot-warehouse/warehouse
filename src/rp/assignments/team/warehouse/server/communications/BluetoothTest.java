54d1bc62851858dbe995633812010c4730522c12
James Bray <jxb1123@student.bham.ac.uk>
2018-03-08 16:32:57 +0000

    Updated to match new route planning/execution

--- src/rp/assignments/team/warehouse/server/communications/BluetoothTest.java
+++ src/rp/assignments/team/warehouse/server/communications/BluetoothTest.java
@@ -3,6 +3,7 @@ package rp.assignments.team.warehouse.server.communications;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import rp.assignments.team.warehouse.server.Location;
 import rp.assignments.team.warehouse.server.route.execution.RouteExecution;
 import rp.assignments.team.warehouse.server.route.planning.AStar;
 import rp.assignments.team.warehouse.server.route.planning.Data;
@@ -11,36 +12,22 @@ import rp.assignments.team.warehouse.server.route.planning.State;
 public class BluetoothTest {
 
     public static void main(String[] args) {
-        Data.addObstacle();
-        State start = new State(2, 2);
-        State goal = new State(3, 2);
-        AStar demo = new AStar(start, goal);
-        demo.findPath();
-        RouteExecution routeExecution = new RouteExecution();
-        Scanner scan = new Scanner(System.in);
         CommunicationsManager manager = new CommunicationsManager(CommunicationsManager.ROBOT_1_NAME,
                 CommunicationsManager.ROBOT_1_ADDRESS);
         manager.startServer();
-        ArrayList<Integer> testOrders = RouteExecution.movementCommands;
-        // t.start();
+        ArrayList<Integer> testOrders = new ArrayList<>();
+    	Location start1 = new Location(0, 0);
+		Location goal1 = new Location(0, 2);
+		RouteExecution.pathForReading = AStar.findPath(start1, goal1);
+		RouteExecution rExecution = new RouteExecution();
+		testOrders = rExecution.getMovements();
+
         if (manager.isConnected()) {
             System.out.println("Enter orders");
             manager.sendOrders(testOrders);
             System.out.println("Orders sent");
         }
-        ;
-//		while(true) {
-//			testOrders.clear();
-//			System.out.println("Send orders");
-//			String str = scan.nextLine();
-//			while(!str.equals("end")) {
-//				testOrders.add(Integer.parseInt(str));
-//				str = scan.nextLine();
-//			}
-//			manager.sendOrders(testOrders);
-//
-//
-//		}
+
 
     }
 
