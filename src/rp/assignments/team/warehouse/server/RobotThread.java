7e6d33e8080c4c27b317770694aaa51198441e8f
Jordan Mussi <jxm1099@cs.bham.ac.uk>
2018-03-10 14:36:37 +0000

    Missed RobotThread in last commit

--- src/rp/assignments/team/warehouse/server/RobotThread.java
+++ src/rp/assignments/team/warehouse/server/RobotThread.java
@@ -3,6 +3,7 @@ package rp.assignments.team.warehouse.server;
 import java.util.ArrayList;
 
 import rp.assignments.team.warehouse.server.communications.CommunicationsManager;
+import rp.assignments.team.warehouse.server.route.execution.Instruction;
 import rp.assignments.team.warehouse.server.route.execution.RouteExecution;
 import rp.assignments.team.warehouse.server.route.planning.AStar;
 
@@ -10,21 +11,21 @@ public class RobotThread extends Thread {
 
 	private Robot robot;
 	private CommunicationsManager commsManager;
-	
+
 	public RobotThread(Robot robot, CommunicationsManager commsManager) {
 		this.robot = robot;
 		this.commsManager = commsManager;
 	}
-	
+
 	@Override
-    public void run() {		
+    public void run() {
     	while (commsManager.isConnected()) {
     		if (robot.getCurrentPick() != null && !robot.hasComputedInstructionsForPick()) {
     			ArrayList<Location> path = AStar.findPath(robot.getCurrentLocation(), robot.getCurrentPick().getPickLocation());
-    			ArrayList<Integer> instructions = RouteExecution.convertCoordinatesToInstructions(robot.getCurrentFacingDirection(), path);
-    			
+    			ArrayList<Instruction> instructions = RouteExecution.convertCoordinatesToInstructions(robot.getCurrentFacingDirection(), path);
+
     			commsManager.sendOrders(instructions);
-    			
+
     			robot.setHasComputedInstructionsForPick(true);
     		}
     	}
