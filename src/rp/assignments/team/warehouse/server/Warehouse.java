b64142e891c011d31bdb74d5d60366d3a146bf20
ajc785 <ajc785@cs.bham.ac.uk>
2018-03-10 12:08:19 +0000

    Renamed method getRobotLocations

--- src/rp/assignments/team/warehouse/server/Warehouse.java
+++ src/rp/assignments/team/warehouse/server/Warehouse.java
@@ -1,14 +1,11 @@
 package rp.assignments.team.warehouse.server;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import rp.assignments.team.warehouse.server.job.Job;
-import rp.assignments.team.warehouse.server.job.selection.IJobSelector;
-import rp.assignments.team.warehouse.server.job.selection.PriorityJobSelector;
 
 // TODO what else is warehouse class going to hold?
 public class Warehouse {
@@ -44,7 +41,7 @@ public class Warehouse {
         return this.robots;
     }
 
-    public Map<String, Location> getLocations() {
+    public Map<String, Location> getRobotLocations() {
         return null; // TODO Get map of robot name and location from list of robots
     }
 
