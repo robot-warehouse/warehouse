ea9018feb120740e41d459ff0a56dbfb4d58b478
ajc785 <ajc785@git.cs.bham.ac.uk>
2018-03-15 18:18:32 +0000

    Added TODOs for RobotThread loop

--- src/rp/assignments/team/warehouse/server/RobotThread.java
+++ src/rp/assignments/team/warehouse/server/RobotThread.java
@@ -28,6 +28,10 @@ public class RobotThread extends Thread {
 	@Override
     public void run() {
     	while (commsManager.isConnected()) {
+            // TODO update location on robot object
+            // TODO check if robot is finished with job
+            // TODO possibly add some reconnect code
+            // TODO send cancellation order
     		if (robot.getCurrentPicks() != null && !robot.hasComputedInstructionsForPick()) {
     			ArrayList<Location> path = AStar.findPath(robot.getCurrentLocation(), ((Pick)robot.getCurrentPicks().toArray()[0]).getPickLocation());
     			ArrayList<Instruction> instructions = RouteExecution.convertCoordinatesToInstructions(robot.getCurrentFacingDirection(), path);
