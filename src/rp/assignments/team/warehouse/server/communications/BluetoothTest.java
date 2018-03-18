1a667ef0855ede5cdd6d941341257c8efe7b8a79
Adam Cox <ajc785@git.cs.bham.ac.uk>
2018-03-18 21:21:56 +0000

    Commented out non-working code
    Communication test classes now require setting more setup so cannot be simply done.
    TwoRobots isn't finished

--- src/rp/assignments/team/warehouse/server/communications/BluetoothTest.java
+++ src/rp/assignments/team/warehouse/server/communications/BluetoothTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import rp.assignments.team.warehouse.server.Location;
+import rp.assignments.team.warehouse.server.Robot;
 import rp.assignments.team.warehouse.server.RobotInfo;
 import rp.assignments.team.warehouse.server.route.execution.RouteExecution;
 import rp.assignments.team.warehouse.server.route.planning.AStar;
@@ -13,31 +14,31 @@ import rp.assignments.team.warehouse.shared.Instruction;
 public class BluetoothTest {
 
 	public static void main(String[] args) {
-		CommunicationsManager manager = new CommunicationsManager(RobotInfo.JOHNCENA);
-		manager.startServer();
-		ArrayList<Instruction> testOrders = new ArrayList<>();
-		Location start1 = new Location(0, 0);
-		Location goal1 = new Location(2, 0);
-		testOrders = RouteExecution.convertCoordinatesToInstructions(Facing.EAST, AStar.findPath(start1, goal1));
-		;
-
-		if (manager.isConnected()) {
-			System.out.println("Enter orders");
-			manager.sendOrders(testOrders);
-			manager.sendNumOfPicks(2);
-			System.out.println("Orders sent");
-		}
-		while (!manager.getFinished()) {
-
-		}
-		Location start2 = new Location(2, 0);
-		Location goal = new Location(4, 0);
-		System.out.println("new orders sent");
-		testOrders = RouteExecution.convertCoordinatesToInstructions(Facing.EAST, AStar.findPath(start2, goal));
-		manager.sendOrders(testOrders);
-		while (!manager.getFinished()) {
-
-		}
+//		CommunicationsManager manager = new CommunicationsManager();
+//		manager.startServer();
+//		ArrayList<Instruction> testOrders = new ArrayList<>();
+//		Location start1 = new Location(0, 0);
+//		Location goal1 = new Location(2, 0);
+//		testOrders = RouteExecution.convertCoordinatesToInstructions(Facing.EAST, AStar.findPath(start1, goal1));
+//		;
+//
+//		if (manager.isConnected()) {
+//			System.out.println("Enter orders");
+//			manager.sendOrders(testOrders);
+//			manager.sendNumOfPicks(2);
+//			System.out.println("Orders sent");
+//		}
+//		while (!manager.getFinished()) {
+//
+//		}
+//		Location start2 = new Location(2, 0);
+//		Location goal = new Location(4, 0);
+//		System.out.println("new orders sent");
+//		testOrders = RouteExecution.convertCoordinatesToInstructions(Facing.EAST, AStar.findPath(start2, goal));
+//		manager.sendOrders(testOrders);
+//		while (!manager.getFinished()) {
+//
+//		}
 	
 		
 	}
