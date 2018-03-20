8752629ba01dbaf2f066b290342fdbdd5dca9d8f
akp798 <akp798@student.bham.ac.uk>
2018-03-20 23:10:40 +0000

    Testing stuff

--- src/rp/assignments/team/warehouse/server/route/planning/Main.java
+++ src/rp/assignments/team/warehouse/server/route/planning/Main.java
@@ -1,27 +1,30 @@
 package rp.assignments.team.warehouse.server.route.planning;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import rp.assignments.team.warehouse.server.Location;
 
 public class Main {
 
 	public static void main(String args[]) {
-		ArrayList<State> l1 = new ArrayList<State>();
-		ArrayList<State> l2 = new ArrayList<State>();
-		ArrayList<State> l3 = new ArrayList<State>();
+		System.out.println("here1");
+		List<Location> l1 = new ArrayList<>();
+		List<Location> l2 = new ArrayList<>();
+		List<Location> l3 = new ArrayList<>();
 
-		State start1 = new State(2,6);
-		State goal1 = new State(0,5);
-		State start2 = new State(0,0);
-		State goal2 = new State(0,7);
-		State start3 = new State(0,3);
-		State goal3 = new State(0,1);
+		Location start1 = new State(2,6);
+		Location goal1 = new State(0,5);
+		Location start2 = new State(0,0);
+		Location goal2 = new State(0,7);
+		Location start3 = new State(0,5);
+		Location goal3 = new State(0,1);
 		
-		l1 = AStar.findPath(start1, goal1, Data.obstacles);
-		l2 = TwoRobots.findPath(l1, start2, goal2);
-		l3 = ThreeRobots.findPath(l1, l2, start3, goal3);
-		
+		l1 = Windowed.findPath(start1, goal1, Data.getObstacles());
 		System.out.println("l1 ----->" + l1);
+		l2 = Windowed.findPath(l1, start2, goal2, Data.getObstacles());
 		System.out.println("l2 ----->" + l2);
+		l3 = Windowed.findPath(l1, l2, start3, goal3, Data.getObstacles());		
 		System.out.println("l3------>" + l3);
 		
 		
