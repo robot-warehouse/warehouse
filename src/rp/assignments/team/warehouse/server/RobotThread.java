f7813e2e591a73d4e10381f4aff8cd50606c07be
Adam Cox <ajc785@git.cs.bham.ac.uk>
2018-03-20 22:51:30 +0000

    RobotThread is now using new multi robot route planning

--- src/rp/assignments/team/warehouse/server/RobotThread.java
+++ src/rp/assignments/team/warehouse/server/RobotThread.java
@@ -5,8 +5,8 @@ import java.util.List;
 import rp.assignments.team.warehouse.server.communications.CommunicationsManager;
 import rp.assignments.team.warehouse.server.route.execution.RouteExecution;
 import rp.assignments.team.warehouse.server.route.planning.AStar;
-import rp.assignments.team.warehouse.shared.Instruction;
 import rp.assignments.team.warehouse.server.route.planning.RoutePlanning;
+import rp.assignments.team.warehouse.shared.Instruction;
 
 public class RobotThread extends Thread {
 
@@ -16,8 +16,8 @@ public class RobotThread extends Thread {
     /** The communication interface with the robot. */
     private CommunicationsManager communicationsManager;
 
-	/** The instance of the route planning class */
-	private RoutePlanning routePlanner;
+    /** The instance of the route planning class */
+    private RoutePlanning routePlanner;
 
     /**
      * @param robot The robot the thread is for.
@@ -47,9 +47,9 @@ public class RobotThread extends Thread {
                 List<Location> path;
 
                 if (!this.robot.hasFinishedPickup()) {
-                    path = AStar.findPath(this.robot.getCurrentLocation(), this.robot.getCurrentPickLocation());
+                    path = this.routePlanner.findPath(this.robot.getCurrentLocation(), this.robot.getCurrentPickLocation());
                 } else if (!this.robot.hasFinishedDropOff()) {
-                    path = AStar.findPath(this.robot.getCurrentLocation(), this.robot.getCurrentDropLocation());
+                    path = this.routePlanner.findPath(this.robot.getCurrentLocation(), this.robot.getCurrentDropLocation());
                 } else {
                     continue; // should never be reached here but wanted to be more explicit with if logic
                 }
@@ -59,8 +59,8 @@ public class RobotThread extends Thread {
                     continue;
                 }
 
-                List<Instruction> instructions = RouteExecution
-                        .convertCoordinatesToInstructions(this.robot.getCurrentFacingDirection(), path);
+                List<Instruction> instructions = RouteExecution.convertCoordinatesToInstructions(
+                    this.robot.getCurrentFacingDirection(), path);
 
                 Location lastLocationInPath = path.get(path.size() - 1);
                 if (lastLocationInPath.equals(this.robot.getCurrentPickLocation())) {
